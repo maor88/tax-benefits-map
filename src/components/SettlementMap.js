7d1beb0e81d6551669a0fa7d8f9c5763b3c8442c
Maoravitan <maor@taranis.ag>
2020-09-19 20:03:56 +0300

    fix

--- src/components/SettlementMap.js
+++ src/components/SettlementMap.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import {API_KEY,GEOCODE_KEY} from '../config.js'
 // import {LOCATIONS} from '../data/locations.js'
@@ -14,62 +13,10 @@ Geocode.setLanguage("iw");
 Geocode.setRegion("il");
 
 
-// const wait = (ms) => {
-//     var start = new Date().getTime();
-//     var end = start;
-//     while(end < start + ms) {
-//       end = new Date().getTime();
-//    }
-//  }
-
-// const chunkArray = (myArray, chunk_size) => {
-//     let index = 0;
-//     const arrayLength = myArray.length;
-//     let tempArray = [];
-    
-//     for (index = 0; index < arrayLength; index += chunk_size) {
-//         let myChunk = myArray.slice(index, index+chunk_size);
-//         // Do something if you want with the group
-//         tempArray.push(myChunk);
-//     }
-
-//     return tempArray;
-// }
-
 
 const SettlementMap = props => {
 
-    // const [locations, setLocations] = useState([])
-    
-    // useEffect (async ()=> {
-    //     const result = chunkArray(LOCATIONS, 90);
-    //     let locations_added = []
-    //     let cnt = 0
-    //     result[5].forEach(async (location) => {
-    //         getLocationByPlaceName(location[0], location[1]).then( res => {
-    //             locations_added.push(res)
-    //             cnt++;
-    //             if(cnt === result[5].length -1){
-    //                 console.log(JSON.stringify(locations_added))
-    //             }
-    //         })
-    //     });
 
-       
-    // },[])
-
-    // const getLocationByPlaceName  = async (name, tax) =>  {
-    //     let location;
-    //     await Geocode.fromAddress(name).then(
-    //         response => {
-    //             location = response.results[0].geometry.location;
-    //         },
-    //         error => {
-    //           console.error(error);
-    //         }
-    //       );
-    //     return { lat: location.lat, lng: location.lng, name:name, tax:tax}
-    // }
 
     const config = {
         center: {
